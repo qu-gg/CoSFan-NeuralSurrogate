Fix exact-replay batch getting, update figure making to include better metric calculation, add MAE and DC metrics

memory/ExactReplay.py
@@ -10,7 +10,12 @@ class ExactReplay(Memory):
         self.args = args
 
         # Dictionary of previous tasks and exemplars
-        self.tasks = dict()
+        self.buffer = {
+            'xs': [],
+            'ys': None,
+            'names': None,
+            'scars': None
+        }
 
     def task_update(self):
         pass
@@ -19,44 +24,93 @@ class ExactReplay(Memory):
         pass
 
     def epoch_update(self, _, task_id, model=None):
-        # Get full dataset for this task
-        xs = model.trainer.train_dataloader.dataset.datasets.xs.to(self.args.devices[0])
-        names = model.trainer.train_dataloader.dataset.datasets.names.to(self.args.devices[0])
-        labels = model.trainer.train_dataloader.dataset.datasets.labels.permute(1, 0).contiguous().to(self.args.devices[0])
-        scars = model.trainer.train_dataloader.dataset.datasets.scars.to(self.args.devices[0])
+        if len(self.buffer['xs']) == 0:
+            self.buffer['xs'] = [x for x in model.trainer.train_dataloader.dataset.datasets.xs.to(self.args.devices[0])]
+            self.buffer['ys'] = model.trainer.train_dataloader.dataset.datasets.labels.permute(1, 0).to(self.args.devices[0])
+            self.buffer['names'] = model.trainer.train_dataloader.dataset.datasets.names.to(self.args.devices[0])
+            self.buffer['labels'] = model.trainer.train_dataloader.dataset.datasets.labels.permute(1, 0).contiguous().to(self.args.devices[0])
+            self.buffer['scars'] = model.trainer.train_dataloader.dataset.datasets.scars.to(self.args.devices[0])
+        else:
+            self.buffer['xs'].extend([x for x in model.trainer.train_dataloader.dataset.datasets.xs.to(self.args.devices[0])])
+            self.buffer['ys'] = torch.vstack((self.buffer['ys'], model.trainer.train_dataloader.dataset.datasets.labels.permute(1, 0).contiguous().to(self.args.devices[0])))
+            self.buffer['names'] = torch.vstack((self.buffer['names'], model.trainer.train_dataloader.dataset.datasets.names.to(self.args.devices[0])))
+            self.buffer['labels'] = torch.vstack((self.buffer['labels'], model.trainer.train_dataloader.dataset.datasets.labels.permute(1, 0).contiguous().to(self.args.devices[0])))
+            self.buffer['scars'] = torch.concatenate((self.buffer['scars'], model.trainer.train_dataloader.dataset.datasets.scars.to(self.args.devices[0])))
+            
+        print(len(self.buffer['xs']))
+        print(self.buffer['ys'].shape)
+        print(self.buffer['names'].shape)
+        print(self.buffer['labels'].shape)
+        print(self.buffer['scars'].shape)
+            
+        print(f"Task Distribution: {torch.unique(self.buffer['scars'], return_counts=True)}")
 
-        if self.args.window is not None:
-            xs = xs[:, :, :self.args.window]
+    # def get_batch(self, k_shot):
+    #     # Select random indices from the reservoir
+    #     sample_indices = np.random.choice(range(len(self.buffer["xs"])), self.args.batch_size // 2, replace=False)
 
-        # Assign to the dictionary
-        self.tasks[task_id] = {
-            'xs': xs,
-            'names': names,
-            'labels': labels,
-            'scars': scars
-        }
-        print(f"Task Distribution: {self.tasks.keys()} {[self.tasks[ti]['xs'].shape for ti in self.tasks.keys()]}")
+    #     scars_base = self.buffer['scars'].cpu()
 
-    def get_batch(self, k_shot):
-        task_size = self.args.batch_size // 2
+    #     # Get the corresponding data
+    #     xs, xs_domains, names, scars = [], [], [], []
+    #     ys, ys_domains = [], []
+    #     for sample_idx in sample_indices:
+    #         # Get query
+    #         xs.append(self.buffer['xs'][sample_idx])
+    #         ys.append(self.buffer['ys'][sample_idx])
+    #         names.append(self.buffer['names'][sample_idx])
+    #         scars.append(self.buffer['scars'][sample_idx])
+            
+    #         # Get context
+    #         domain_indices = np.where(scars_base == scars_base[sample_idx])[0]
+    #         domain_indices = domain_indices[np.where(domain_indices != sample_idx)[0]]
+    #         domain_sample_indices = np.random.choice(domain_indices, k_shot, replace=True)
+            
+    #         xs_domains.append(torch.stack([self.buffer['xs'][idx] for idx in domain_sample_indices]))
+    #         ys_domains.append(torch.stack([self.buffer['ys'][idx] for idx in domain_sample_indices]))
         
-        # Build outputs of each task
+    #     # Stack the domains
+    #     names = torch.vstack(names)
+    #     scars = torch.vstack(scars)
+    #     # print(torch.unique(scars), )
+    #     return xs, xs_domains, ys, ys_domains, names, scars
+    
+    def get_batch(self, k_shot):
+        # Get unique task IDs and create task-specific indices
+        scars_base = self.buffer['scars'].cpu()
+        unique_tasks = torch.unique(scars_base)
+        task_indices = {int(task): torch.where(scars_base == task)[0].numpy() for task in unique_tasks}
+        
+        # Sample tasks with replacement for batch_size // 2 queries
+        sampled_tasks = np.random.choice(unique_tasks.numpy(), size=self.args.batch_size // 2, replace=True)
+        
+        # Initialize lists to store batch data
         xs, xs_domains, names, scars = [], [], [], []
         ys, ys_domains = [], []
-        task_sequence = np.random.choice(len(self.tasks.keys()), size=task_size, replace=True)
-        for task_id in task_sequence:
-            index = np.random.choice(range(self.tasks[task_id]['xs'].shape[0]), 1 + self.args.domain_size, replace=False)
-            
-            xs.append(self.tasks[task_id]['xs'][index][0])
-            ys.append(self.tasks[task_id]['labels'][index][0])
-            names.append(self.tasks[task_id]['names'][index][0])
-            scars.append(self.tasks[task_id]['scars'][index][0])
-            
-            xs_domains.append(self.tasks[task_id]['xs'][index][1:1 + k_shot])
-            ys_domains.append(self.tasks[task_id]['labels'][index][1:1 + k_shot])
-            
-        print(scars)
         
+        for task_id in sampled_tasks:
+            # Get indices for this task
+            task_specific_indices = task_indices[int(task_id)]
+            
+            # Sample one query example from this task
+            query_idx = np.random.choice(task_specific_indices, size=1)[0]
+            
+            # Get query data
+            xs.append(self.buffer['xs'][query_idx])
+            ys.append(self.buffer['ys'][query_idx])
+            names.append(self.buffer['names'][query_idx])
+            scars.append(self.buffer['scars'][query_idx])
+            
+            # Sample k_shot context examples from the same task (excluding query)
+            context_indices = task_specific_indices[task_specific_indices != query_idx]
+            domain_sample_indices = np.random.choice(context_indices, k_shot, replace=True)
+            
+            # Get context data
+            xs_domains.append(torch.stack([self.buffer['xs'][idx] for idx in domain_sample_indices]))
+            ys_domains.append(torch.stack([self.buffer['ys'][idx] for idx in domain_sample_indices]))
+        
+        # Stack the domains
         names = torch.vstack(names)
         scars = torch.vstack(scars)
+        
         return xs, xs_domains, ys, ys_domains, names, scars

utils/metrics.py
@@ -3,13 +3,22 @@
 
 Holds a variety of metric computing functions for time-series forecasting models
 """
-import scipy.stats as stats
 import numpy as np
+import scipy.stats as stats
+
+from skimage.filters import threshold_otsu
 
 
 def mse(output, target, **kwargs):
-    """ Gets the mean of the per-pixel MSE for the given length of timesteps used for training """
+    """ Gets the mean of the per-step MSE for the given length of timesteps used for training """
     full_pixel_mses = (output - target) ** 2
+    sequence_pixel_mse = np.mean(full_pixel_mses, axis=(1, 2))
+    return sequence_pixel_mse, np.mean(sequence_pixel_mse), np.std(sequence_pixel_mse)
+
+
+def mae(output, target, **kwargs):
+    """ Gets the mean of the per-step MAE for the given length of timesteps used for training """
+    full_pixel_mses = np.abs(output - target)
     sequence_pixel_mse = np.mean(full_pixel_mses, axis=(1, 2))
     return sequence_pixel_mse, np.mean(sequence_pixel_mse), np.std(sequence_pixel_mse)
 
@@ -50,3 +59,23 @@ def scc(u, x, **kwargs):
         res.append(correlation_sum)
     res = np.array(res)
     return res, np.mean(res), np.std(res)
+
+
+def dcc(u, x):
+    m, n, w = u.shape
+    dice_cc = []
+
+    for i in range(m):
+        u_row = u[i, :, 50]
+        x_row = x[i, :, 50]
+
+        thresh_u = threshold_otsu(u_row)
+        u_scar_idx = np.where(u_row >= thresh_u)[0]
+        thresh_x = threshold_otsu(x_row)
+        x_scar_idx = np.where(x_row >= thresh_x)[0]
+
+        intersect = set(u_scar_idx) & set(x_scar_idx)
+        dice_cc.append(2 * len(intersect) / float(len(set(u_scar_idx)) + len(set(x_scar_idx))))
+
+    dice_cc = np.array(dice_cc)
+    return dice_cc